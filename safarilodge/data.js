ac159808484804ed790da4c11f662910056b0a8c
msimbao <mphatsosimbao@gmail.com>
2026-05-27 11:36:59 +0200

    Added Safari Lodge

---
+++ safarilodge/data.js
@@ -0,0 +1,210 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-courtyard",
+      "name": "Courtyard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1774,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.484973442119447,
+          "pitch": 0.022593852558646432,
+          "rotation": 0,
+          "target": "1-bedrooms"
+        },
+        {
+          "yaw": -1.1211774929891796,
+          "pitch": 0.020138800337111462,
+          "rotation": 0,
+          "target": "2-common-area"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.336590880959182,
+          "pitch": -0.04087004326646593,
+          "title": "<strong data-start=\"33\" data-end=\"43\">Title:</strong> Safari Jeep Rides",
+          "text": "\nExperience guided safari jeep rides and explore wildlife, nature, and scenic landscapes up close. A must-do adventure during your stay."
+        },
+        {
+          "yaw": 0.15208295128423366,
+          "pitch": 0.050498694469311545,
+          "title": "Welcome to the Lobby",
+          "text": "\nRelax in our spacious open lounge designed for comfort and conversation. Enjoy a calm atmosphere with modern seating, natural lighting, and easy access to all guest facilities."
+        },
+        {
+          "yaw": 2.9883476758105116,
+          "pitch": 0.12032137267244813,
+          "title": "Scenic Garden Entrance",
+          "text": "Step outside to enjoy beautifully maintained gardens and peaceful outdoor surroundings ideal for morning walks and fresh air."
+        }
+      ]
+    },
+    {
+      "id": "1-bedrooms",
+      "name": "Bedrooms",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 887,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.42364813578779,
+          "pitch": 0.13307423531324503,
+          "rotation": 0,
+          "target": "0-courtyard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6354014972063595,
+          "pitch": 0.35190760017342626,
+          "title": "Ensuite Bathroom",
+          "text": "Enjoy a clean and modern private bathroom equipped with hot water, fresh towels, and essential toiletries."
+        },
+        {
+          "yaw": 1.1655505783702758,
+          "pitch": -0.3856773988913105,
+          "title": "Climate Comfort",
+          "text": "Stay comfortable throughout your stay with air conditioning designed to keep the room cool and relaxing."
+        },
+        {
+          "yaw": -1.1952813178936168,
+          "pitch": 0.19294071357730758,
+          "title": "Relaxing Outdoor View",
+          "text": "\nTake in peaceful outdoor views and natural light from the comfort of your room."
+        },
+        {
+          "yaw": -0.20187117607165916,
+          "pitch": 0.023969010637394206,
+          "title": "Premium Bedding",
+          "text": "Our soft bedding and premium mattresses are carefully prepared to ensure a restful and refreshing sleep experience."
+        },
+        {
+          "yaw": 1.405507293767302,
+          "pitch": 0.12012720194923432,
+          "title": "Workspace",
+          "text": "A quiet and comfortable workspace is available for guests who need to work remotely, study, or plan their travels."
+        },
+        {
+          "yaw": -1.8285173119428002,
+          "pitch": -0.2167724030618281,
+          "title": "Original African Art",
+          "text": "All artworks are original pieces by local Zambian and Southern African artists. Prints and similar works are available for purchase at reception."
+        }
+      ]
+    },
+    {
+      "id": "2-common-area",
+      "name": "Common Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 887,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0842808362004135,
+          "pitch": 0.11227156733336408,
+          "rotation": 0,
+          "target": "0-courtyard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5895449540601927,
+          "pitch": -0.216213781366843,
+          "title": "Refreshments",
+          "text": "Enjoy complimentary fruit drinks, tea, and light refreshments throughout the day in a peaceful and welcoming environment."
+        },
+        {
+          "yaw": 0.09643095100174648,
+          "pitch": 0.16768015203907716,
+          "title": "Breakfast &amp; Dining Area",
+          "text": "<div>Meals are served here in the communal area.&nbsp;</div><div><br></div><div>Breakfast 07:00–09:30 ·</div><div><br></div><div>&nbsp;Lunch 13:00–14:30 ·</div><div><br></div><div>&nbsp;Supper 19:00–21:00.&nbsp;</div><div><br></div><div>Guests may use the lounge to relax, read, or socialise at any time of day</div>"
+        },
+        {
+          "yaw": -0.620620826054326,
+          "pitch": -0.02011304835014549,
+          "title": "Social Lounge",
+          "text": "Meet friends, relax after a long day, or enjoy casual conversations in our comfortable lounge area. Perfect for reading, working, or unwinding."
+        },
+        {
+          "yaw": 3.033169403150664,
+          "pitch": 0.001982291375696832,
+          "title": "Welcome to the Lobby",
+          "text": "Relax in our spacious open lounge designed for comfort and conversation. Enjoy a calm atmosphere with modern seating, natural lighting, and easy access to all guest facilities."
+        },
+        {
+          "yaw": 1.5871877794220701,
+          "pitch": -0.15842070809322806,
+          "title": "African Art collection",
+          "text": "All artworks are original pieces by local Zambian and Southern African artists. Prints and similar works are available for purchase at reception."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
